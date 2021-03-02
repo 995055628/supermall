d1103f3876e0dc64c6db29474782f71e26422070
fengyankun.name <995055628@qq.com>
2021-03-02 12:08:49 +0800

    2021年3月2日12:08:03

---
+++ src/components/common/tabbar/TabBarItem.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="tab-bar-item" @click="itemClick">
+    <div v-if="!isActive">
+      <slot name="item-icon"></slot>
+    </div>
+    <div v-else>
+      <slot name="item-icon-active"></slot>
+    </div>
+    <div :style="activeStyle">
+      <slot name="item-text"></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TabBarItem",
+  props: {
+    path: String,
+    activeColor: {
+      type: String,
+      default: 'red'
+    }
+  },
+  data() {
+    return {
+      // isActive: true
+    }
+  },
+  computed:{
+    isActive() {
+      console.log(this.$route.path)
+      return this.$route.path === this.path
+    },
+    activeStyle() {
+      return this.isActive ? {color: this.activeColor} : {}
+    }
+  },
+  methods: {
+    itemClick() {
+      this.$router.replace(this.path).catch(err => {
+        console.log('输出报错',err)
+      })
+    }
+  }
+}
+</script>
+
+  <style scoped>
+  .tab-bar-item {
+    flex: 1;
+    text-align: center;
+    height: 49px;
+    font-size: 14px;
+  }
+
+  .tab-bar-item img {
+    width: 18px;
+    height: 18px;
+    margin-top: 2px;
+    vertical-align: middle;
+  }
+
+</style>
